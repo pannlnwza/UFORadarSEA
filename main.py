39bf54d81313afafb364d336f6b8f2d1d1d7009c
Pattapon Gowanit <iampattapon@gmail.com>
2024-05-04 19:59:05 +0700

    Added docstrings

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
-from uforadar_dataprocessor import UFODataProcessor
-from uforadar_ui import App
+from data_processor import UFODataProcessor
+from uforadar_ui import UFOApp
 
 
 def main():
+    """
+    Main function to run the UFO Radar application.
+    """
     current_dir = os.getcwd()
     ufo_data = os.path.join(current_dir, 'data', 'nuforc_data.csv')
     airport_data = os.path.join(current_dir, 'data', 'gadb_country_declatlon.csv')
     data_processor = UFODataProcessor(ufo_data, airport_data)
-    app = App(data_processor)
+    app = UFOApp(data_processor)
     app.run()
 
 
