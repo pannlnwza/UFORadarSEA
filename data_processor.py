5f4bab6c4e35d9aa64379d7c91d0660b75aff1b9
Pattapon Gowanit <iampattapon@gmail.com>
2024-05-04 19:57:06 +0700

    Added docstrings

--- data_processor.py
+++ data_processor.py
@@ -1,47 +1,158 @@
+import os
 import datetime
+from math import radians, sin, cos, sqrt, atan2
 import pandas as pd
-import numpy as np
 
 
 class UFODataProcessor:
+    """
+    A class for processing UFO data.
+    """
+
     def __init__(self, ufo_reports_file, airports_file):
+        """
+        Initialize UFODataProcessor class.
+
+        :param ufo_reports_file: Path to the UFO reports CSV file.
+        :type ufo_reports_file: pd.Dataframe
+        :param airports_file: Path to the airports CSV file.
+        :type airports_file: pd.Dataframe
+        """
         self.ufo_reports = pd.read_csv(ufo_reports_file)
         self.airports = pd.read_csv(airports_file)
+        self.new_row = {}
 
-    def separate_datetime(self):
-        # Separate date_time_found into year_found, month, and hour
-        self.ufo_reports['date_time_found'] = pd.to_datetime(self.ufo_reports['date_time_found'])
-        self.ufo_reports['year_found'] = self.ufo_reports['date_time_found'].dt.year
-        self.ufo_reports['month'] = self.ufo_reports['date_time_found'].dt.month
-        self.ufo_reports['hour'] = self.ufo_reports['date_time_found'].dt.hour
+    def get_ufo_data(self) -> pd.DataFrame:
+        """
+        Get UFO sighting data.
 
-    def haversine_distance(self, lat1, lon1, lat2, lon2):
-        # Calculate the great-circle distance between two points
-        R = 6371  # Radius of the Earth in kilometers
-        lat1, lon1, lat2, lon2 = map(np.radians, [lat1, lon1, lat2, lon2])
+        :return: DataFrame containing UFO sighting data.
+        :rtype: pandas.DataFrame
+        """
+        return self.ufo_reports
+
+    def find_nearest_airport(self, latitude: float, longitude: float, airport_data) -> float:
+        """
+        Find the nearest airport to a given location.
+
+        :param latitude: Latitude of the location.
+        :type latitude: float
+        :param longitude: Longitude of the location.
+        :type longitude: float
+        :param airport_data: DataFrame containing airport data.
+        :type airport_data: pandas.DataFrame
+        :return: Minimum airport distance.
+        :rtype: float
+        """
+        min_distance = float('inf')
+
+        for _, airport in airport_data.iterrows():
+            airport_lat = airport['LAT']
+            airport_lon = airport['LONG']
+            distance = self.haversine_distance(latitude, longitude, airport_lat, airport_lon)
+            if distance < min_distance:
+                min_distance = distance
+        return min_distance
+
+    def save_to_csv(self, date_time_found, country, location, latitude, longitude, ufo_shape,
+                    length_of_encounter_seconds, description):
+        """
+        Save UFO sighting data to a CSV file.
+        """
+        report_no = self.ufo_reports.loc[len(self.ufo_reports)-1]['report_no'] + 1
+        min_distance = self.find_nearest_airport(float(latitude),
+                                                                  float(longitude),
+                                                                  self.airports)
+        year_found, month, hour = self.separate_datetime(date_time_found)
+        season = self.month_to_season(month)
+        date_documented = datetime.date.today().strftime('%m/%d/%Y')
+        self.new_row = {'report_no': report_no,
+                        'date_documented': date_documented,
+                        'date_time_found': date_time_found,
+                        'year_found': year_found,
+                        'month': month,
+                        'hour': hour,
+                        'season': season,
+                        'country': country,
+                        'location': location,
+                        'latitude': latitude,
+                        'longitude': longitude,
+                        'UFO_shape': ufo_shape,
+                        'length_of_encounter_seconds': length_of_encounter_seconds,
+                        'distance_to_nearest_airport_km': min_distance,
+                        'description': description.strip()
+                        }
+        self.ufo_reports.loc[len(self.ufo_reports)] = self.new_row
+        current_dir = os.getcwd()
+        ufo_data = os.path.join(current_dir, 'data', 'nuforc_data.csv')
+        self.ufo_reports.to_csv(ufo_data, mode='w', index=False)
+
+    @staticmethod
+    def separate_datetime(date_time) -> tuple:
+        """
+        Separate datetime into year, month, and hour.
+
+        :param date_time: Datetime string.
+        :type date_time: str
+        :return: Year, month, and hour.
+        :rtype: tuple
+        """
+        date_time = pd.to_datetime(date_time)
+        year_found = date_time.year
+        month_found = date_time.month
+        hour_of_the_day = date_time.hour
+        return year_found, month_found, hour_of_the_day
+
+    @staticmethod
+    def month_to_season(month: int) -> str:
+        """
+        Convert month to season.
+
+        :param month: Month.
+        :type month: int
+        :return: Season corresponding to the month.
+        :rtype: str
+        """
+        if month in [3, 4, 5]:
+            return 'Spring'
+        if month in [6, 7, 8]:
+            return 'Summer'
+        if month in [9, 10, 11]:
+            return 'Autumn/Fall'
+        return 'Winter'
+
+    @staticmethod
+    def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+        """
+        Calculate the Haversine distance between two points on the Earth's surface.
+
+        :param lat1: Latitude of the first point in degrees.
+        :type lat1: float
+        :param lon1: Longitude of the first point in degrees.
+        :type lon1: float
+        :param lat2: Latitude of the second point in degrees.
+        :type lat2: float
+        :param lon2: Longitude of the second point in degrees.
+        :type lon2: float
+        :return: Distance between the two points in kilometers.
+        :rtype: float
+        """
+        # Radius of the Earth in kilometers
+        R = 6371.0
+
+        # Convert latitude and longitude from degrees to radians
+        lat1 = radians(float(lat1))
+        lon1 = radians(float(lon1))
+        lat2 = radians(float(lat2))
+        lon2 = radians(float(lon2))
+
+        # Calculate differences in latitude and longitude
         dlat = lat2 - lat1
         dlon = lon2 - lon1
-        a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
-        c = 2 * np.arcsin(np.sqrt(a))
-        distance = R * c
+
+        # Haversine formula to calculate distance
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        distance = R * c  # Distance in kilometers
         return distance
-
-    def find_nearest_airport_distance(self):
-        # Function to find the distance of each UFO report to the nearest airport
-        def nearest_airport_distance(row):
-            min_distance = float('inf')
-            for _, airport in self.airports.iterrows():
-                distance = self.haversine_distance(row['latitude'], row['longitude'], airport['latitude_deg'],
-                                                   airport['longitude_deg'])
-                min_distance = min(min_distance, distance)
-            return min_distance
-
-        self.ufo_reports['distance_to_nearest_airport_km'] = self.ufo_reports.apply(nearest_airport_distance, axis=1)
-
-    def save_to_csv(self, filename):
-        # Save the processed UFO report data to a CSV file
-        self.ufo_reports['date_documented'] = datetime.date.today().strftime('%m/%d/%Y')
-        self.ufo_reports.to_csv(filename, index=False)
-
-    def get_ufo_data(self):
-        return self.ufo_reports
