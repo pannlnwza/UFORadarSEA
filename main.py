389a72739fb1415455995eca077ce672557709c3
Pattapon Gowanit <iampattapon@gmail.com>
2024-05-11 20:50:52 +0700

    Ignore FutureWarnings

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 from data_processor import UFODataProcessor
 from uforadar_ui import UFOApp
+import warnings
 
 
 def main():
     """
     Main function to run the UFO Radar application.
     """
+    # Ignore Future Warnings
+    warnings.simplefilter(action="ignore", category=FutureWarning)
     current_dir = os.getcwd()
     ufo_data = os.path.join(current_dir, 'data', 'nuforc_data.csv')
     airport_data = os.path.join(current_dir, 'data', 'gadb_country_declatlon.csv')
